e872f840b919778fe0aecda11a64eb311c910832
Alex Canchanya <alexandercanchanya@cnea.gob.ar>
2024-08-23 15:32:58 +0000

    Replace pylint.py

--- pylint.py
+++ pylint.py
@@ -1,8 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 15 15:11:17 2024
+
+@author: Alex
+"""
+
+# import glob
 import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 from datetime import datetime
 import copy
+# from Codigos_py.Repositorio.Activacion import redondeo, ajuste_pol
+
 
 class data_sead:
     def __init__(self, path_SEAD, fecha_ensayo):
@@ -41,7 +51,7 @@ class data_sead:
         plt.close(99)
         return step_index
     
-    def per_step(self, det, path_step = None):
+    def per_step(self, det, path_step = None, power_list = list(range(50))):
         try:
             step_times = np.loadtxt(path_step)
         except:
@@ -51,8 +61,8 @@ class data_sead:
 
         for jj, interval in enumerate(step_times):
             cond_sead = np.logical_and(self.time>interval[0], self.time<interval[1])
-            data_step[jj] = self.CIC_Marcha(det)[cond_sead]
-            time_step[jj] = self.time[cond_sead]
+            data_step[power_list[jj]] = self.CIC_Marcha(det)[cond_sead]
+            time_step[power_list[jj]] = self.time[cond_sead]
         return time_step, data_step
     
     
@@ -66,54 +76,57 @@ def process_lint(path, t_inicio):
     return time, counts, cod
 
 class data_LINT:
-    def __init__(self, path_LINT_list, path_step, t_inicio, save_data=False):
+    def __init__(self, path_LINT_list, path_step, t_inicio, save_data=False, power_list = False):
         step_times = np.loadtxt(path_step)
-        counts_cond = {ii:[] for ii in range(len(step_times))}
-        time_cond = {ii:[] for ii in range(len(step_times))}
-        cod_Ts = {ii:[] for ii in range(len(step_times))}
+        if power_list:
+            potencias = power_list
+        else:
+            potencias = list(range(len(step_times)))
+        step_times = np.loadtxt(path_step)
+        counts_cond = {ii:[] for ii in potencias}
+        time_cond = {ii:[] for ii in potencias}
+        cod_Ts = {ii:[] for ii in potencias}
         for path in path_LINT_list:
             time, counts, cod = process_lint(path, t_inicio)
             for jj, interval in enumerate(step_times):
                 cond = np.logical_and(time>interval[0], time<interval[1])
                 if any(cond) == True:    
-                    counts_cond[jj] += counts[cond].to_list()
-                    time_cond[jj] = np.append(time_cond[jj], time[cond])
-                    cod_Ts[jj] += cod[cond].to_list()
-        for jj in counts_cond:
-            counts_cond[jj] = np.array(counts_cond[jj])
+                    counts_cond[potencias[jj]] += counts[cond].to_list()
+                    time_cond[potencias[jj]] = np.append(time_cond[potencias[jj]], time[cond])
+                    cod_Ts[potencias[jj]] += cod[cond].to_list()
+        for pot in potencias:
+            counts_cond[pot] = np.array(counts_cond[pot])
         self.counts_cond = counts_cond
         self.time_cond = time_cond
         self.cod = cod_Ts
+        self.potencias = potencias
         if save_data:
             np.save('time_cond_conrampas.npy', time_cond)
             np.save('counts_cond_conrampas.npy', counts_cond) 
             np.save('cod_Ts_conrampas.npy', cod_Ts)
-
-    def copy(self):
-        return copy.deepcopy(self)
-
+            
     def apply_corr(self, ncond=2):
         time_norep = {}
         counts_norep = {}
-        for i in self.time_cond:
-            cond = np.array([cod!=10 for cod in self.cod[i]])
+        for pot in iter(self.potencias):
+            cond = np.array([cod!=10 for cod in self.cod[pot]], dtype='bool')
             if cond.any() == True:
-                t_rep, counts_rep = self.time_cond[i][cond], self.counts_cond[i][cond]
-                time_norep[i] = self.time_cond[i][~cond]
-                counts_norep[i] = self.counts_cond[i][~cond]
+                t_rep, counts_rep = self.time_cond[pot][cond], self.counts_cond[pot][cond]
+                time_norep[pot] = self.time_cond[pot][~cond]
+                counts_norep[pot] = self.counts_cond[pot][~cond]
                 t_rep_unique = pd.unique(t_rep)
                 _, i_start, nrep = np.unique(t_rep, return_index=True, return_counts=True)
                 res = np.split(counts_rep, i_start[1:])
                 res_cond = [len(xx)>=ncond for xx in res]
-                time_norep[i] = np.append(time_norep[i], t_rep_unique[res_cond])
-                counts_norep[i] = np.append(counts_norep[i], np.array([np.mean(r)/0.1 for r in np.array(res, dtype='object')[res_cond]]))   
+                time_norep[pot] = np.append(time_norep[pot], t_rep_unique[res_cond])
+                counts_norep[pot] = np.append(counts_norep[pot], np.array([np.mean(r)/0.1 for r in np.array(res, dtype='object')[res_cond]]))   
             else:
-                time_norep[i] = self.time_cond[i]
-                counts_norep[i] = self.counts_cond[i]
+                time_norep[pot] = self.time_cond[pot]
+                counts_norep[pot] = self.counts_cond[pot]
         self.time_norep  = time_norep
         self.counts_norep = counts_norep
     
-    def moving_avg(self, N_vent = 10, N_sep = 3):
+    def moving_avg(self, N_vent = 10, N_sep =3):
         try:
             self.time_norep
             self.counts_norep
@@ -121,58 +134,70 @@ class data_LINT:
             self.apply_corr()
         time_deriva = {}
         counts_deriva = {}
-        for i in self.time_norep:
-            i_cut = np.array(np.where(np.diff(self.time_norep[i])>=1/60*N_sep)[0] + 1, dtype = 'int64')
-            t_cut, counts_cut = np.split(self.time_norep[i], i_cut), np.split(self.counts_norep[i], i_cut)
-            counts_deriva[i] = np.zeros((1, 2))
-            time_deriva[i] = np.zeros(1)
+        for pot in self.potencias:
+            i_cut = np.array(np.where(np.diff(self.time_norep[pot])>=1/60*N_sep)[0] + 1, dtype = 'int64')
+            t_cut, counts_cut = np.split(self.time_norep[pot], i_cut), np.split(self.counts_norep[pot], i_cut)
+            counts_deriva[pot] = np.zeros((1, 2))
+            time_deriva[pot] = np.zeros(1)
             for time, count in zip(t_cut, counts_cut):
                 if len(count) > N_vent:
                     k = 0
                     while k < len(count) - N_vent:
-                        counts_deriva[i] = np.row_stack((counts_deriva[i], np.array([np.mean(count[k:k+N_vent]), np.std(count[k:k+N_vent], ddof=1)])))
+                        counts_deriva[pot] = np.row_stack((counts_deriva[pot], np.array([np.mean(count[k:k+N_vent]), np.std(count[k:k+N_vent], ddof=1)])))
                         k+=1
-                    time_deriva[i] = np.append(time_deriva[i], time[:-N_vent])
+                    time_deriva[pot] = np.append(time_deriva[pot], time[:-N_vent])
                 elif len(count) == N_vent:
-                    counts_deriva[i] = np.row_stack((counts_deriva[i], [np.mean(count), np.std(count, ddof=1)] ))
-                    time_deriva[i] = np.append(time_deriva[i], time[0])
+                    counts_deriva[pot] = np.row_stack((counts_deriva[pot], [np.mean(count), np.std(count, ddof=1)] ))
+                    time_deriva[pot] = np.append(time_deriva[pot], time[0])
                 else:
                     continue
-            time_deriva[i] = np.delete(time_deriva[i], 0, axis=0)
-            counts_deriva[i] = np.delete(counts_deriva[i], 0, axis=0)
+            time_deriva[pot] = np.delete(time_deriva[pot], 0, axis=0)
+            counts_deriva[pot] = np.delete(counts_deriva[pot], 0, axis=0)
         return time_deriva, counts_deriva
 
-
+def average_stable(time, signal, step_times):
+    potencias = signal.keys()
+    counts_avg = np.zeros((len(potencias), 2))
+    for jj, pot in enumerate(potencias):
+        cond = np.logical_and(time[pot]>step_times[jj][0], time[pot]<step_times[jj][1])
+        counts_avg[jj] = [np.mean(signal[pot][cond]), np.std(signal[pot][cond], ddof=1)]
+    return counts_avg
 
 #%% Figures 
 
-def figure_SEAD(datos_sead, marchas_list, yscale = 'log', nfig = 1):
+def figure_SEAD(datos_sead, marchas_list, yscale = 'log', nfig = 99):
     fig = plt.figure(nfig)
     for marcha in marchas_list:
         plt.plot(datos_sead.time, datos_sead.CIC_Marcha(marcha), '.', label = marcha)
+    plt.suptitle('Exported data from SEAD. Start time = {}'.format(datos_sead.t_inicio))
     plt.xlabel('t [min]')
     plt.ylabel('Current [A]')
     plt.legend()
     plt.grid(ls='--')
     plt.yscale(yscale)
     plt.tight_layout()  
-    plt.close()
+    plt.show()
     return fig
 
 def figure_LINT_err(LINT_t, LINT_counts, yscale = 'symlog',
                     power_list = [], nfig = 98):
     fig = plt.figure(nfig)
-    axs = []
-    for t, count in zip(LINT_t.values(), LINT_counts.values()):
-        ax = plt.errorbar(t, count[:, 0], yerr= count[:, 1], fmt='.')
-        axs.append(ax)
-    plt.xlabel('t [min]')
-    plt.ylabel('CPS')
-    plt.yscale(yscale)
-    if power_list!=[]:
-        plt.legend(axs, power_list)
-    plt.grid(ls='--')
-    plt.tight_layout()  
-    plt.close()
-    return fig
+    ax = fig.add_subplot()
+    axs_list = []
+    for t, count, pot in zip(LINT_t.values(), LINT_counts.values(), LINT_counts.keys()):
+        ax.errorbar(t, count[:, 0], yerr= count[:, 1], fmt='.', label = pot + 'W')
+        axs_list.append(ax)
+    ax.set_xlabel('t [min]')
+    ax.set_label('CPS')
+    ax.set_yscale(yscale)
+    # if power_list!=[]:
+        # plt.legend(axs, power_list)
+    ax.grid(ls='--')
+    fig.tight_layout()  
+    fig.show()
+    return fig, ax
 
+def add_vlines(fig, ax, list_x, style = '--'):
+    y0, y1 = ax.get_ylim()
+    ax.vlines(list_x, ymin = y0, ymax = y1, ls = style, color = 'tab:gray')
+    return fig
